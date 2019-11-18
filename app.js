50c624a8e074b8803911067bc356339d614b514e
EC2 Default User <ec2-user@ip-172-31-95-107.ec2.internal>
2019-11-18 00:17:09 +0000

    Commits Homework 4 code

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const express = require("express");
+const fakeData = require("faker");
+
 const app = express();
 app.engine('html', require('ejs').renderFile)
 app.use(express.static("public"));
 
 //routes
 app.get("/", function(req, res){
-    res.render("index.html");
+    const phrase = fakeData.hacker.phrase();
+    res.render("index.ejs", {title: 'CyberSecurity',
+        phrase});
 });
 
 app.get("/elements", function(req, res){
-   res.render("elements.html"); 
+   const phrase = fakeData.hacker.phrase();
+    res.render("elements.ejs", {title: 'Elements',
+        phrase}); 
 });
 
 app.get("/purpose", function(req, res){
-    res.render("purpose.html"); 
+    const phrase = fakeData.hacker.phrase();
+    res.render("purpose.ejs", {title: 'Purpose',
+        phrase});
 });
 
 app.get("/threats", function(req, res){
-    res.render("threats.html"); 
+    const phrase = fakeData.hacker.phrase();
+    res.render("threats.ejs", {title: 'Threats',
+        phrase}); 
 });
 
 //server listener
